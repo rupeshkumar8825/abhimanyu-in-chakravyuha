26dc5605c3065621c59b398df9e63e24355e9d54
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2024-05-21 19:26:57 +0530

    Feature : Implement the solution and add new test cases along with assumptions

--- code/solution.cpp
+++ code/solution.cpp
@@ -18,25 +18,32 @@ void updateEnemyPowerList(vector<int> &enemyPowerList)
     return;
 }
 
-// function to actually solve the problem for this purpose 
+
+
 bool checkAbhimanyuCanCross(vector<int> &enemyPowerList, int totalNumberOfSkips, int totalNumberOfRecharge, int initialPower)
 {
-    // since at 3rd and 7th circle the enemies can regenerate themselves with half of their initial power 
-    // and can attack Abhimanyu at the 4th and the 8th circle
-    // Hence we can update the enemy power for the 4th enemy and the 8th enemy
+    /**
+     * @brief Update the array
+     * since at 3rd and 7th circle the enemies can regenerate themselves with half of their initial power 
+     * and can attack Abhimanyu at the 4th and the 8th circle
+     * Hence we can update the enemy power for the 4th enemy and the 8th enemy
+     */
     updateEnemyPowerList(enemyPowerList);
+
 
     int currRemainingPower = initialPower;
     int totalNumberOfEnemies = enemyPowerList.size();
 
-    // this means that the power is not enough to fight 
-    // we can have two options either to recharge or to skip the fight 
-    // using the greedy technique explained below: 
-    //     1. If abhimanyu can get enough power to fight with the current enemy by just recharging 
-    //        then he should recharge instead of skipping the fight. 
-    //
-    //     2. Abhimanyu should skip only when after recharging also he will not be able to pass the
-    //        circle. 
+    /**
+     * @brief Approach
+     * When Abhimanyu will not enough power to fight then 
+     * we can have two options either to recharge or to skip the fight 
+     * using the greedy technique explained below: 
+     *      1. If abhimanyu can get enough power to fight with the current enemy by just recharging 
+     *         then he should recharge instead of skipping the fight. 
+     *      2. Abhimanyu should skip only when after recharging also he will not be able to pass the
+     *         circle.
+     */
 
 
     for(int i = 0; i<totalNumberOfEnemies; i++)
@@ -45,7 +52,7 @@ bool checkAbhimanyuCanCross(vector<int> &enemyPowerList, int totalNumberOfSkips,
 
         if(currRemainingPower >= currEnemyPower)
         {
-            // then abhimanyu can pass this circle 
+            /* then abhimanyu can pass this circle */
             currRemainingPower = currRemainingPower - currEnemyPower;
             continue;
         }
@@ -53,34 +60,73 @@ bool checkAbhimanyuCanCross(vector<int> &enemyPowerList, int totalNumberOfSkips,
 
         if((initialPower >= currEnemyPower) && (totalNumberOfRecharge > 0))
         {
-            // Abhimanyu will be able to pass the circle after recharging. 
-            // then it is better to recharge and pass the circle.
+            /*
+                Abhimanyu will be able to pass the circle after recharging. 
+                then it is better to recharge and pass the circle instead of skipping.
+            */
             currRemainingPower = initialPower - currEnemyPower;
             totalNumberOfRecharge = totalNumberOfRecharge - 1;
         }
         else if(totalNumberOfSkips > 0)
         {
-            // this means that he is not able to pass the circle even after recharging hence skipping is the way
+            /*this means that he is not able to pass the circle even after recharging hence skipping is the way*/
             totalNumberOfSkips = totalNumberOfSkips - 1;
 
-            // no change in current remaining power
+            /*no change in current remaining power*/
         }
         else
         {
-            // otherwise there are no ways in which he can pass the circle hence returning false 
+            /* finally no ways in which he can pass the circle hence returning false */
             return false;
         }
     } 
 
 
 
-    // if control reaches here this means he was able to cross all the circles 
+    /* finally he was able to cross all the circles */ 
     return true;
 
 }
 
+
+
+
+void displayOutput(vector<int> &enemyPowerList, int totalNumberOfSkips, int totalNumberOfRecharge, int initialPower, bool canCross)
+{
+    cout << "For input : " << endl; 
+    cout << "Initial Power          p  : " << initialPower << endl;
+    cout << "Total Skips Allowed    a  : " << totalNumberOfSkips << endl;
+    cout << "Total Recharge Allowed b  : " << totalNumberOfRecharge << endl;
+    cout << "List of enemy's Power  k[]: ";
+
+    for(int i = 0;i<enemyPowerList.size();i++)
+    {
+        cout << enemyPowerList[i] << " ";
+    }
+
+
+    cout << endl;
+    cout << "Output : " << boolalpha << canCross << endl;
+    cout << "----------------------------------------------------------------" << endl; 
+    cout << "----------------------------------------------------------------" << endl;
+
+
+}
+
+
+
+
+
 int main()
 {
+    /**
+     * @brief 
+     * Following are the assumptions for this problem
+     *  1. When Abhimanyu recharge his power then his power will become as initial power p
+     *  2. Power values cannot be negative.
+     *  3. a, b and k[] will also consists of positive integers
+     *  4. Array size is fixed i.e 11 
+     */
     int totalNumberOfEnemies = 11;
     int a = 0;
     int b = 0;
@@ -93,11 +139,63 @@ int main()
     p = 20;
 
     enemyPowerList = {8, 2, 4, 10, 12, 16, 18, 5, 2, 7, 6};
-
     bool canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
+
+    // test case 2
+    a = 2;
+    b = 3;
+    p = 20;
+
+    enemyPowerList = {8, 2, 4, 10, 12, 16, 18, 5, 2, 7, 6};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
+
+    // test case 3
+    a = 2;
+    b = 3;
+    p = 20;
+
+    enemyPowerList = {8, 2, 4, 10, 12, 16, 18, 5, 2, 7, 6};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
+
+    // test case 4
+    a = 0;
+    b = 10;
+    p = 20;
+
+    enemyPowerList = {8, 2, 4, 10, 12, 16, 18, 5, 2, 7, 6};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
 
 
-    cout << canPassCircles << endl;
+    // test case 5
+    a = 0;
+    b = 0;
+    p = 0;
+
+    enemyPowerList = {8, 2, 4, 10, 12, 16, 18, 5, 2, 7, 6};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
+
+    // test case 6
+    a = 0;
+    b = 0;
+    p = 0;
+
+    enemyPowerList = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
+    
+    // test case 7
+    a = 0;
+    b = 0;
+    p = 66;
+
+    enemyPowerList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    canPassCircles = checkAbhimanyuCanCross(enemyPowerList, a, b, p);
+    displayOutput(enemyPowerList, a, b, p, canPassCircles);
 
     // say everything went fine 
     return 0;
